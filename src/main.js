815e9cd953fee6f5b27a24aa372a533b44bf85de
Gonzalez Acosta Daniel Alberto <dgonza25@cantv.com.ve>
2018-03-19 18:37:30 -0400

    creacion del componente about

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue' 
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+import './assets/vendor/font-awesome/css/font-awesome.min.css'
+import './assets/vendor/devicons/css/devicons.min.css'
+import './assets/vendor/simple-line-icons/css/simple-line-icons.css'
+import './assets/css/resume.min.css'
 
 Vue.config.productionTip = false
 
